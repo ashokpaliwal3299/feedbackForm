05521c00eb5a39dcd81e063654ea23d3e8fd288e
ashokpaliwal3299 <ashokpaliwal3299@gmail.com>
2024-11-19 15:19:17 +0530

    schduelr for remainder active

--- src/main/java/com/feedback/form/service/EmailSchedulerService.java
+++ src/main/java/com/feedback/form/service/EmailSchedulerService.java
@@ -86,15 +86,15 @@ public class EmailSchedulerService {
 		return remainingSites;
 	}
 
-	//@Scheduled(cron = "0 00 18 19 * ?")
-//	public void checkRemainSiteAndReminder() {
-//		System.out.println("scheduler runing...");
-//		List<ClientSiteMaster> remainSites = remainigClientSites();
-//
-//		for (ClientSiteMaster site : remainSites) {
-//			triggerRemainderEmails(site, url + site.getId());
-//		}
-//	}
+	@Scheduled(cron = "0 00 18 19 * ?")
+	public void checkRemainSiteAndReminder() {
+		System.out.println("scheduler runing...");
+		List<ClientSiteMaster> remainSites = remainigClientSites();
+
+		for (ClientSiteMaster site : remainSites) {
+			triggerRemainderEmails(site, url + site.getId());
+		}
+	}
 
 	private void triggerRemainderEmails(ClientSiteMaster site, String url) {
 		if (site != null && site.getEmail() != null) {
